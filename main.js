ff02fe23d847e9f0b33581042fbb976fda1cf6ea
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-20 12:26:18 +0100

    START

---
+++ main.js
@@ -0,0 +1,6 @@
+let lampadina = document.getElementById("lampadina")
+let button = document.getElementById("buttonAccensione")
+
+button.addEventListener ("click", function(){
+    lampadina.src = "./img/yellow_lamp.png"
+})
